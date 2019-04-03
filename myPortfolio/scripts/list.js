f4ed1e31aeebb22d92a8aff8ffd7dc5aefca3c93
jaimeeherrera <jaimeeherrera22@gmail.com>
2019-04-03 15:36:39 -0500

    added dashboard

--- myPortfolio/scripts/list.js
+++ myPortfolio/scripts/list.js
@@ -1,4 +1,4 @@
-alert("message message");
+alert("This is a Test ALERT- NO NEED TO WORRY ;)");
 $(function () {
 
     let $list = $('ul');
@@ -17,3 +17,31 @@ $(function () {
     });
 
 });
+
+var myLocation = document.getElementById("my-coordinates")
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(displayPosition)
+    } else {
+        myLocation.innerHTML = "Not available"
+    }
+}
+
+function displayPosition(position) {
+    myLocation.innerHTML = "Longitude: " + position.coords.longitude + " Latitude: " + position.coords.latitude
+}
+
+function startTime() {
+    var today = new Date();
+    var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    document.getElementById('clk1').innerHTML = today.getHours() + " : ";
+    document.getElementById('clk2').innerHTML = today.getMinutes() + " : ";
+    document.getElementById('clk3').innerHTML = today.getSeconds();
+    document.getElementById("day").innerHTML = today.getDate() + " /";
+    document.getElementById('month').innerHTML = today.getMonth() + 1 + " /";
+    document.getElementById('year').innerHTML = today.getFullYear();
+    var t = setTimeout(function () { startTime() }, 500);
+    document.getElementById("weekdays").innerHTML = "Today is " + weekdays[today.getDay()];
+}
+startTime();
